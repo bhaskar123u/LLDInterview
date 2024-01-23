3bd165e1e7b43c6f724b68be51153001a2248146
Bhaskar Sharan <bhaskar123u@gmail.com>
2024-01-24 01:45:10 +0530

    Refactoring

---
+++ ShoppingCart/src/ShoppingApp.java
@@ -0,0 +1,19 @@
+// Using decorator design pattern
+// Different products can have different coupons, design a shopping cart that computes final net payable for list of product in cart
+public class ShoppingApp {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Jio");
+    System.out.println();
+
+    ShoppingCart shoppingCart = new ShoppingCart();
+
+    Product item1 = new Shoe("Adidas", 200, ProductType.FOOTWEAR);
+    Product item2 = new Tshirt("Levis", 100, ProductType.APPAREL);
+
+    shoppingCart.addToCart(item1);
+    shoppingCart.addToCart(item2);
+
+    System.out.println("Your total bill amount is : " + shoppingCart.getTotalOriginalAmount());
+    System.out.println("Net payable amount is : " + shoppingCart.getTotalPrice());
+  }
+}
