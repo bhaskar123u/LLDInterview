86173d8211015a6c53c2f584558a7828938eea10
Bhaskar Sharan <bhaskar123u@gmail.com>
2024-01-27 02:26:07 +0530

    LLD for VendingMachine

---
+++ VendingMachine/src/Item.java
@@ -0,0 +1,18 @@
+public class Item {
+  
+  ItemType itemType;
+  int price;
+
+  public ItemType getItemType() {
+    return itemType;
+  }
+  public void setItemType(ItemType itemType) {
+    this.itemType = itemType;
+  }
+  public int getPrice() {
+    return price;
+  }
+  public void setPrice(int price) {
+    this.price = price;
+  }
+}
---
+++ VendingMachine/src/ItemType.java
@@ -0,0 +1,6 @@
+public enum ItemType {
+  COKE,
+  PEPSI,
+  JUICE,
+  SODA;
+}
